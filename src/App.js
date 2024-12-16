846bee686acf80716b48eae13eb0900928ec837c
aryan0 motwani <motwaniaryan7.com>
2024-12-16 13:41:59 +0530

    second

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello This is the best</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Git Updated</h1>
     </div>
   );
 }
