3edaf25bbca438fad6f1468ee2aba6f5795d5a73
aryan0 motwani <motwaniaryan7.com>
2024-12-16 15:07:42 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Form from './Form';
+import "./index.css"
+// import SignatureCollection from './SigantureCollection';
+// import TicketsPage from './Ticket';
+import Admin from './Admin';
+import Records from './Record';
+// import Reports from './Reports';
+// import Form2 from './Form2';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Git Updated</h1>
-    </div>
+      <Routes>
+        <Route path="/form" element={<Form />} />
+        <Route path="/admin" element={<Admin />} />
+        {/* <Route path="/ticket/:id" element={<SignatureCollection />} /> */}
+        {/* <Route path="/tickets" element={<TicketsPage />} /> */}
+        <Route path="/records" element={<Records />} />
+        {/* <Route path="/reports" element={<Reports />} /> */}
+        {/* <Route path="/form2" element={<Form2 />} /> */}
+        {/* Optional: Redirect unknown routes to /form */}
+        <Route path="*" element={<Navigate to="/form" replace />} />
+      </Routes>
   );
 }
 
